c1a84d6b16cb8d974aa78e17623a222398710ce5
AdemilsonViana <ademilson.junior@dominiumfinanceiro.com.br>
2025-04-16 20:27:27 -0300

    inicial configs and imports

---
+++ main.py
@@ -0,0 +1,14 @@
+#%% ----------------------------------------------
+# imports
+#streamlit run WorkLoadReport.py
+import pandas as pd
+import streamlit as st
+
+from Functions.APIs.notion import API_Notion
+
+#%% ----------------------------------------------
+# Extract data
+
+
+#%% ----------------------------------------------
+# streamlit
